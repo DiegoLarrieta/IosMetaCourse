886b723326a2ab68d84e2343b806d630da2700f0
Diego <diegolarrieta@gmail.com>
2025-01-17 19:45:59 -0600

    Initial Commit

---
+++ LittleLemmonMeta/LittleLemmonMetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemmonMetaApp.swift
+//  LittleLemmonMeta
+//
+//  Created by Luis Diego Dominguez Larrieta on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemmonMetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LittleLemmonMetaTests/LittleLemmonMetaTests.swift
@@ -0,0 +1,16 @@
+//
+//  LittleLemmonMetaTests.swift
+//  LittleLemmonMetaTests
+//
+//  Created by Luis Diego Dominguez Larrieta on 17/01/25.
+//
+
+import Testing
+
+struct LittleLemmonMetaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
